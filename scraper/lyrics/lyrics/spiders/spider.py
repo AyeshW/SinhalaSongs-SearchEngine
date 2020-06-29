7d0d7cb83c8b09cd60d1ea140ba6f278773120b5
AyeshW <ayeshweerasinghe.16@cse.mrt.ac.lk>
2020-06-29 21:16:43 +0100

    Add guitar key attribute

--- scraper/lyrics/lyrics/spiders/spider.py
+++ scraper/lyrics/lyrics/spiders/spider.py
@@ -1,5 +1,4 @@
 from scrapy.spiders import SitemapSpider
-from datetime import datetime
 
 
 class SinhalaLyricsSpider(SitemapSpider):
@@ -11,7 +10,6 @@ class SinhalaLyricsSpider(SitemapSpider):
 
     def sitemap_filter(self, entries):
         for entry in entries:
-            #date_time = datetime.strptime(entry['lastmod'], '%Y-%m-%d')
             year = entry['lastmod'].split('-')[0]
             if year >= '2017':
                 yield entry
@@ -20,7 +18,7 @@ class SinhalaLyricsSpider(SitemapSpider):
 
     def parse(self, response):
         song_lines = response.xpath('//*[@class="su-column-inner su-u-clearfix su-u-trim"]/pre/text()').getall()
-        # guitar = response.xpath('//*[@class="entry-content"]/h3/text()').get()
+        guitar = response.xpath('//*[@class="entry-content"]/h3/text()').get()
         en_title = response.xpath('//*[@class="entry-content"]/h2/text()').get()
         sn_title = response.xpath('//*[@class="entry-content"]/h2/span[@class="sinTitle"]/text()').get()
         genre = response.xpath('//*[@class="entry-tags"]/a/text()').get()
@@ -35,12 +33,13 @@ class SinhalaLyricsSpider(SitemapSpider):
             if not sn_title:
                 sn_title = en_title
 
-            # if guitar:
-            #     key = guitar.split(' | ')[0].split(': ')[1]
-            #     beat = guitar.split(' | ')[1].split(': ')[1]
-            # else:
-            #     key = None
-            #     beat = None
+            key = ""
+            if guitar:
+                keys = guitar.split(': ')
+                if len(keys) > 1:
+                    key = keys[1].split(' |')[0]
+                else:
+                    key = keys[0]
 
             for line in song_lines:
                 song = song + " " + line.replace("\n", " ").replace("\t", " ")
@@ -52,8 +51,7 @@ class SinhalaLyricsSpider(SitemapSpider):
                 'singer': singer,
                 'writer': writer,
                 'music_artist': music,
-                # 'key': key,
-                # 'beat': beat,
+                'key': key,
                 'lyrics': song,
                 'views': int((views.split('- ')[1].split('V')[0]).
                              replace(',', ''))
