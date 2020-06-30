bcb53ea6ad89217dd1ffa003b1ca11145b344467
AyeshW <ayeshweerasinghe.16@cse.mrt.ac.lk>
2020-06-30 07:43:26 +0100

    Add search queries

---
+++ searchengine/queries.py
@@ -0,0 +1,106 @@
+import json
+
+aggregations = {
+			"Sinhala Singer Filter": {
+				"terms": {
+					"field": "sn_singer.keyword",
+					"size": 15
+				}
+			},
+            "Sinhala Writer Filter": {
+				"terms": {
+					"field": "sn_writer.keyword",
+					"size": 15
+				}
+			},
+            "Sinhala Musician Filter": {
+                "terms": {
+                    "field": "sn_music_artist.keyword",
+                    "size": 15
+                }
+            },
+			"Sinhala Genre Filter": {
+				"terms": {
+					"field": "sn_genre.keyword",
+					"size": 15
+				}
+			},
+            "English Singer Filter": {
+				"terms": {
+					"field": "en_singer.keyword",
+					"size": 15
+				}
+			},
+            "English  Writer Filter": {
+				"terms": {
+					"field": "en_writer.keyword",
+					"size": 15
+				}
+			},
+            "English  Musician Filter": {
+                "terms": {
+                    "field": "en_music_artist.keyword",
+                    "size": 15
+                }
+            },
+			"English  Genre Filter": {
+				"terms": {
+					"field": "en_genre.keyword",
+					"size": 15
+				}
+			}
+}
+
+
+def exact_match_query(phrase, field):
+    query = {
+        "query": {
+            "match_phrase": {
+                field: phrase
+            }
+        }
+    }
+
+    query = json.dumps(query)
+    return query
+
+
+def faceted_multi_match_query(phrase, fields):
+    # queries are with weighted boosted fields
+    query = {
+        "size": 100,
+        "query": {
+            "multi_match": {
+                "query": phrase,
+                "fields": fields,
+                "operator": 'or'
+            }
+        },
+        "aggs": aggregations
+
+    }
+
+    query = json.dumps(query)
+    return query
+
+
+def faceted_multi_match_range(phrase, results_count, fields):
+    query = {
+        "size": results_count,
+        "sort": [
+            {"views": {"order": "desc"}},
+        ],
+        "query": {
+            "multi_match": {
+                "query": phrase,
+                "fields": fields,
+                "operator": 'or'
+            }
+        },
+        "aggs": aggregations
+    }
+
+    query = json.dumps(query)
+    return query
+
+
